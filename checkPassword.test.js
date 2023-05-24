bafa65304f06cc1cf93e88fd4207c425491c7c7e
GardyVoir <clement.wilfart@gmail.com>
2023-05-24 16:11:15 +0200

    test: :test_tube: Added special character test

--- checkPassword.test.js
+++ checkPassword.test.js
@@ -29,13 +29,25 @@ it("should display number error if don't contain a number", () => {
 });
 
 it("should display capital error if don't contain a capital", () => {
-    expect(checkPassword("")).toContain(
-      "Le mot de passe doit contenir au moins une lettre majuscule"
-    );
-    expect(checkPassword("abc")).toContain(
-      "Le mot de passe doit contenir au moins une lettre majuscule"
-    );
-    expect(checkPassword("Abc")).not.toContain(
-      "Le mot de passe doit contenir au moins une lettre majuscule"
-    );
-  });
+  expect(checkPassword("")).toContain(
+    "Le mot de passe doit contenir au moins une lettre majuscule"
+  );
+  expect(checkPassword("abc")).toContain(
+    "Le mot de passe doit contenir au moins une lettre majuscule"
+  );
+  expect(checkPassword("Abc")).not.toContain(
+    "Le mot de passe doit contenir au moins une lettre majuscule"
+  );
+});
+
+it("should display special character error if don't contain a special character", () => {
+  expect(checkPassword("")).toContain(
+    "Le mot de passe doit contenir au moins un caractère spécial"
+  );
+  expect(checkPassword("abc")).toContain(
+    "Le mot de passe doit contenir au moins un caractère spécial"
+  );
+  expect(checkPassword("@bc")).not.toContain(
+    "Le mot de passe doit contenir au moins un caractère spécial"
+  );
+});
