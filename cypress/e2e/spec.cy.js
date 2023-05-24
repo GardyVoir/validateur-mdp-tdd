2379af568e6b2e5b4c666d2eb4c829ea3dd5ace8
GardyVoir <clement.wilfart@gmail.com>
2023-05-24 14:32:45 +0200

    feat: :test_tube: Add cypress test

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,25 @@
+describe('test', () => {
+  it('should display an error if password not valid', () => {
+    cy.visit("http://localhost:5173");
+
+    cy.get("#password").type("abc");
+    cy.get("#btn").click();
+    cy.get("#result").contains("Le mot de passe doit comporter au moins 8 caractères");
+
+    cy.get("#password").type("abcdefghij");
+    cy.get("#btn").click();
+    cy.get("#result").contains("Le mot de passe doit contenir au moins 1 chiffre");
+
+    cy.get("#password").type("abcdefghij1");
+    cy.get("#btn").click();
+    cy.get("#result").contains("Le mot de passe doit contenir au moins une lettre majuscule");
+    
+    cy.get("#password").type("abcdEfghij1");
+    cy.get("#btn").click();
+    cy.get("#result").contains("Le mot de passe doit contenir au moins un caractère spécial");
+    
+    cy.get("#password").type("@bcdEfghij1");
+    cy.get("#btn").click();
+    cy.get("#result").contains("OK");
+  })
+})
